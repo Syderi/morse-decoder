19f0caa031c56a73d1861c1f96432a2858b37b2b
Syderi <syderi@yahoo.com>
2022-07-14 20:47:04 +0300

    fix: decoder

--- src/index.js
+++ src/index.js
@@ -37,10 +37,80 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+// const expr1 = "0010101010   0000000010   0010111010   0010111010   0000111111
+    // ********** 0000101111   0000111111   0000101110   0010111010   0000111010";
+
+// const expr1 = "00101010100000000010001011101000101110100000111111**********00001011110000111111000010111000101110100000111010";
+
 function decode(expr) {
-    // write your solution here
+    // console.log('10-------------------', expr);
+    let result = '';
+    
+   
+for (let j = 0; j < expr.length; j = j +10) {
+    let decodeWordsMors = '';
+    let decodeSimbol = '';
+    let simbolTwo = '';
+
+    let simbol = expr.substr(j, 10);
+    // console.log('20 /////////', simbol);
+
+        if (simbol === '**********') {
+            // console.log('15 *****////', simbol);
+            // console.log('16 *****////', decodeWordsMors);
+            result = result + ' ';
+            // decodeWordsMors = '';
+        } else {
+                for (let i = 0; i < 10; i = i + 2) {
+            // console.log(25, simbolTwo);
+            simbolTwo = simbol.substr(i, 2);
+            // console.log(30, simbolTwo);
+            // simbol = simbol.substr(i+2,10);
+            // console.log(35, simbol);
+
+    if (simbolTwo === '10') {
+        decodeSimbol = '.';
+        } else if (simbolTwo === '11') {
+             decodeSimbol = '-';
+            } else if (simbolTwo === '00') {
+                decodeSimbol = '';
+                };
+
+    //  console.log(40, decodeSimbol);    
+     decodeWordsMors = decodeWordsMors + decodeSimbol;
+
+
+      
+        
+    };
+        };
+
+        // console.log(50, decodeWordsMors);
+
+        if (MORSE_TABLE[decodeWordsMors] !== undefined) {
+              result = result + MORSE_TABLE[decodeWordsMors];
+        }
+
+                                  
+
+
+        //  console.log(60, result);
+
+
+};
+return result;
 }
+
+
+
+// decode(expr1);
 
 module.exports = {
     decode
 }
+
+// 10 stands for dot(.), 11 stands for dash(-).
+
+
+
+        // const result = "hello world";
